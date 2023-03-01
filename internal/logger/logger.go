5de7ef351fec6594e29e967e3b1950abe527b514
Viacheslav Zalitskyi <slaw.z2014@gmail.com>
2023-03-01 20:59:24 +0200

    Added logger.go (uber-go/zap)

---
+++ internal/logger/logger.go
@@ -0,0 +1,66 @@
+package logger
+
+import (
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+type configModifierFunc func(*zap.Config)
+
+func New(configModifiers ...configModifierFunc) (*zap.Logger, error) {
+
+	logger, err := zap.NewDevelopment()
+	cfg := zap.Config{
+		Encoding:         "json",
+		OutputPaths:      []string{"stderr"},
+		ErrorOutputPaths: []string{"stderr"},
+		Level:            zap.NewAtomicLevelAt(zap.InfoLevel),
+		EncoderConfig:    zap.NewProductionEncoderConfig(),
+	}
+
+	for _, c := range configModifiers {
+		c(&cfg)
+	}
+
+	logger, err = cfg.Build()
+	if err != nil {
+		return nil, err
+	}
+
+	return logger, nil
+}
+
+// WithLevel zap.InfoLevel as default
+func WithLevel(level zap.AtomicLevel) configModifierFunc {
+	return func(config *zap.Config) {
+		config.Level = level
+	}
+}
+
+// WithEncoding "json" as default
+func WithEncoding(encoding string) configModifierFunc {
+	return func(config *zap.Config) {
+		config.Encoding = encoding
+	}
+}
+
+// WithDevelopment false as default
+func WithDevelopment(dev bool) configModifierFunc {
+	return func(config *zap.Config) {
+		config.Development = dev
+	}
+}
+
+// WithOutputPaths "stderr" as default
+func WithOutputPaths(paths []string) configModifierFunc {
+	return func(config *zap.Config) {
+		config.OutputPaths = paths
+	}
+}
+
+// WithEncoderConfig as default NewDevelopment
+func WithEncoderConfig(encoder zapcore.EncoderConfig) configModifierFunc {
+	return func(config *zap.Config) {
+		config.EncoderConfig = encoder
+	}
+}
