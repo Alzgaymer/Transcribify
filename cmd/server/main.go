974e556d824a5e056a4303b76535128899aabf32
Viacheslav Zalitskyi <slaw.z2014@gmail.com>
2023-03-02 17:17:56 +0200

    server update. Added get method to receive yt video id

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -15,6 +15,7 @@ import (
 )
 
 func main() {
+	// Setting up server configuration (port etc.)
 	var configuration config.AppConfiguration
 	err := cleanenv.ReadConfig("route.env", &configuration)
 	if err != nil {
@@ -25,9 +26,9 @@ func main() {
 
 	log, err := logger.New(
 		logger.WithDevelopment(true),
-		logger.WithEncoding("console"),
 		logger.WithLevel(zap.NewAtomicLevelAt(zap.DebugLevel)),
 	)
+
 	if err != nil {
 		panic(err)
 	}
@@ -36,6 +37,7 @@ func main() {
 	sig := make(chan os.Signal, 1)
 	signal.Notify(sig, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
 
+	// Waiting to signal from os
 	go func() {
 		defer cancel()
 		<-sig
@@ -43,6 +45,7 @@ func main() {
 		timeout, _ := context.WithTimeout(ctx, 30*time.Second)
 
 		go func() {
+			// Waiting until context is done then panic
 			<-timeout.Done()
 			if timeout.Err() == context.DeadlineExceeded {
 				log.Fatal("Graceful shutdown timed out... forcing exit...")
@@ -57,7 +60,9 @@ func main() {
 		}
 
 	}()
+
 	log.Info("Server is running", zap.String("port", configuration.Port))
+
 	err = server.ListenAndServe()
 	if err != nil && err != http.ErrServerClosed {
 		log.Fatal("Failed to serve", zap.Error(err))
@@ -68,11 +73,34 @@ func main() {
 
 func service() http.Handler {
 
+	log, err := logger.New()
+	if err != nil {
+		return nil
+	}
+
 	router := chi.NewRouter()
 
 	router.Get("/api/v1/hello", func(writer http.ResponseWriter, request *http.Request) {
 		writer.Write([]byte("<h1>Hello</h1>"))
 	})
 
+	videoIDPattern := "[a-zA-Z0-9_-]{11}"
+
+	// Create a route for the GET method that accepts the video ID as a parameter
+	router.Get("/api/v1/{videoID:"+videoIDPattern+"}", func(w http.ResponseWriter, r *http.Request) {
+
+		// Get the video ID from the URL parameters
+		videoID := chi.URLParam(r, "videoID")
+
+		// Do something with the video ID, for example print it to the console
+		log.Info("Input parametr",
+			zap.String("Video ID", videoID),
+			zap.String("URL", "/api/v1/"+videoID),
+		)
+
+		// Return a response to the client
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Video ID: " + videoID))
+	})
 	return router
 }
