65d1932af20e9f7c788602e49cfb92b26a9ee483
Viacheslav Zalitskyi <slaw.z2014@gmail.com>
2023-03-07 02:01:25 +0200

    better error handling

--- routes/routes.go
+++ routes/routes.go
@@ -3,43 +3,93 @@ package routes
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/go-chi/chi/v5"
 	"go.uber.org/zap"
 	"io"
 	"net/http"
+	"regexp"
 	"yt-video-transcriptor/config"
 	"yt-video-transcriptor/models"
-
-	"yt-video-transcriptor/logger"
 )
 
+type Route struct {
+	logger *zap.Logger
+	client *http.Client
+}
+
+func NewRoute(logger *zap.Logger, client *http.Client) *Route {
+	return &Route{logger: logger, client: client}
+}
+
+type MyRequest struct {
+	videoID, language string
+}
+
 // GetVideoTranscription Handle GET request for video with specified language
-func GetVideoTranscription(w http.ResponseWriter, r *http.Request) {
-
-	log, err := logger.New(
-		logger.WithDevelopment(true),
-	)
-
-	if err != nil {
-		return
-	}
+func (route *Route) GetVideoTranscription(w http.ResponseWriter, r *http.Request) {
 
 	// Get the video ID and language from the URL parameters
-	videoID := chi.URLParam(r, "videoID")
-	language := chi.URLParam(r, "language")
 
-	log.Info("Input parameter",
+	videoID := r.URL.Query().Get("v")
+	language := r.URL.Query().Get("lang")
+
+	route.logger.Info("Input parameter",
 		zap.String("Video ID", videoID),
 		zap.String("Language", language),
 		zap.String("URL", r.URL.Path),
 	)
+	matchedVideo, err := regexp.MatchString("^[a-zA-Z0-9_-]{11}$", videoID)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	matchedLang, err := regexp.MatchString("^[a-zA-Z]{2}$", language)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	if !matchedVideo || !matchedLang {
+		w.WriteHeader(http.StatusLengthRequired)
+		return
+	}
+
+	//Make request to API
+	res, err := route.callbackToApi(MyRequest{
+		videoID:  videoID,
+		language: language,
+	})
+	if err != nil {
+		route.logger.Error("Failed to get transcription", zap.Error(err))
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	defer res.Body.Close()
+
+	//Reading response.Body into []model.YTVideo
+	video, err := route.responseToYTVideo(res)
+	if err != nil {
+		route.logger.Error("Failed to unmarshal data", zap.Error(err))
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	err = route.writeVideoJson(w, video)
+	if err != nil {
+		route.logger.Error("Failed to encode data", zap.Error(err))
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+}
+
+func (route *Route) callbackToApi(request MyRequest) (*http.Response, error) {
 
 	configuration := config.GetAPI()
 
 	url := fmt.Sprintf(
 		"https://youtube-transcriptor.p.rapidapi.com/transcript?video_id=%s&lang=%s",
-		videoID,
-		language,
+		request.videoID,
+		request.language,
 	)
 
 	req, _ := http.NewRequest(http.MethodGet, url, nil)
@@ -47,40 +97,21 @@ func GetVideoTranscription(w http.ResponseWriter, r *http.Request) {
 	req.Header.Add("X-RapidAPI-Key", configuration.Key)
 	req.Header.Add("X-RapidAPI-Host", configuration.API)
 
-	res, err := http.DefaultClient.Do(req)
-	if err != nil {
-		log.Error("Failed to get transcription", zap.Error(err))
-	}
-	defer res.Body.Close()
-
-	//Reading response.Body into []model.YTVideo
-	video, err := responseToYTVideo(res)
-	if err != nil || len(video) == 0 {
-		log.Error("Failed to unmarshal data", zap.Error(err))
-		w.WriteHeader(http.StatusNoContent)
-		return
-	}
-
-	err = writeVideoJson(w, video)
-	if err != nil {
-		log.Error("Failed to encode data", zap.Error(err))
-		w.WriteHeader(http.StatusNotFound)
-	}
-	w.WriteHeader(http.StatusOK)
-
+	return route.client.Do(req)
 }
 
-func responseToYTVideo(res *http.Response) ([]models.YTVideo, error) {
+func (route *Route) responseToYTVideo(res *http.Response) ([]models.YTVideo, error) {
 
 	var data []models.YTVideo
 	err := json.NewDecoder(res.Body).Decode(&data)
 	if err != nil {
 		return nil, err
 	}
+
 	return data, nil
 }
 
-func writeVideoJson(w io.Writer, video []models.YTVideo) error {
+func (route *Route) writeVideoJson(w io.Writer, video []models.YTVideo) error {
 
 	encoder := json.NewEncoder(w)
 	encoder.SetIndent("", "    ")
