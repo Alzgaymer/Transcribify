8d365275021baf0f3859597857905ab56f5d90ec
Viacheslav Zalitskyi <slaw.z2014@gmail.com>
2023-03-07 02:01:06 +0200

    changed api route

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,37 +2,43 @@ package main
 
 import (
 	"context"
+	"errors"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"go.uber.org/zap"
+	"log"
 	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
 	"time"
 	"yt-video-transcriptor/config"
-	"yt-video-transcriptor/logger"
+	"yt-video-transcriptor/logging"
 	"yt-video-transcriptor/routes"
 )
 
 func main() {
-	// Setting up server configuration (port etc.)
-	configuration := config.GetRoute()
 
-	server := http.Server{Addr: ":" + configuration.Port, Handler: service()}
-
-	log, err := logger.New(
-		logger.WithDevelopment(true),
-		logger.WithLevel(zap.NewAtomicLevelAt(zap.DebugLevel)),
+	var (
+		configuration = config.GetRoute()
+		client        = &http.Client{
+			Timeout: 30 * time.Second,
+		}
+		ctx, cancel = context.WithCancel(context.Background())
 	)
 
+	logger, err := logging.New(
+		logging.WithDevelopment(true),
+		logging.WithLevel(zap.NewAtomicLevelAt(zap.DebugLevel)),
+	)
 	if err != nil {
-		panic(err)
+		log.Fatal(err.Error())
 	}
-	ctx, cancel := context.WithCancel(context.Background())
+
+	server := http.Server{Addr: ":" + configuration.Port, Handler: service(logger, client)}
 
 	sig := make(chan os.Signal, 1)
-	signal.Notify(sig, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+	signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
 
 	// Waiting to signal from os
 	go func() {
@@ -44,41 +50,38 @@ func main() {
 		go func() {
 			// Waiting until context is done then panic
 			<-timeout.Done()
-			if timeout.Err() == context.DeadlineExceeded {
-				log.Fatal("Graceful shutdown timed out... forcing exit...")
+			if errors.Is(timeout.Err(), context.DeadlineExceeded) {
+				logger.Fatal("Graceful shutdown timed out... forcing exit...")
 			}
 		}()
 
-		log.Info("Stopping server")
+		logger.Info("Stopping server")
 
 		err = server.Shutdown(timeout)
 		if err != nil {
-			log.Fatal("Failed to shutdown server", zap.Error(err))
+			logger.Fatal("Failed to shutdown server", zap.Error(err))
 		}
 	}()
 
-	log.Info("Server is running", zap.String("port", configuration.Port))
+	logger.Info("Server is running", zap.String("port", configuration.Port))
 
 	err = server.ListenAndServe()
 	if err != nil && err != http.ErrServerClosed {
-		log.Fatal("Failed to serve", zap.Error(err))
+		logger.Fatal("Failed to serve", zap.Error(err))
 	}
 
 	<-ctx.Done()
 }
 
-func service() http.Handler {
+func service(logger *zap.Logger, client *http.Client) http.Handler {
 
 	router := chi.NewRouter()
 
 	router.Use(middleware.Logger)
 
+	route := routes.NewRoute(logger, client)
 	// Create a route for the GET method that accepts the video ID as a parameter
-	router.Route("/api/v1", func(r chi.Router) {
-		r.Route("/{videoID:[a-zA-Z0-9_-]{11}}", func(r chi.Router) {
-			r.Get("/{language:[a-zA-Z]{2}}", routes.GetVideoTranscription)
-		})
-	})
+	router.Get("api/v1/videos", route.GetVideoTranscription)
 
 	return router
 }
