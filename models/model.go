cfe63e67461f710b3a2d2de2f6da4c71a6a4d71c
Viacheslav Zalitskyi <slaw.z2014@gmail.com>
2023-03-08 21:55:55 +0200

    implemented custom json unmarshalling

--- models/model.go
+++ models/model.go
@@ -1,5 +1,10 @@
 package models
 
+import (
+	"encoding/json"
+	"strconv"
+)
+
 type YTVideo struct {
 	Title           string          `json:"title"`
 	Description     string          `json:"description"`
@@ -16,7 +21,42 @@ type Thumbnails struct {
 }
 
 type Transcription struct {
-	Subtitle any     `json:"subtitle"`
+	Subtitle string  `json:"subtitle"`
 	Start    float64 `json:"start"`
 	Dur      float64 `json:"dur"`
+}
+
+func (t *Transcription) UnmarshalJSON(data []byte) error {
+	type rawString struct {
+		Subtitle string  `json:"subtitle"`
+		Start    float64 `json:"start"`
+		Dur      float64 `json:"dur"`
+	}
+
+	rawStr := new(rawString)
+
+	err := json.Unmarshal(data, rawStr)
+	if err == nil {
+		t.Dur = rawStr.Dur
+		t.Start = rawStr.Start
+		t.Subtitle = rawStr.Subtitle
+		return nil
+	}
+
+	type rawInt struct {
+		Subtitle int     `json:"subtitle"`
+		Start    float64 `json:"start"`
+		Dur      float64 `json:"dur"`
+	}
+	rawI := rawInt{}
+
+	err = json.Unmarshal(data, &rawI)
+	if err != nil {
+		return err
+	}
+
+	t.Start = rawI.Start
+	t.Dur = rawI.Dur
+	t.Subtitle = strconv.Itoa(rawI.Subtitle)
+	return nil
 }
