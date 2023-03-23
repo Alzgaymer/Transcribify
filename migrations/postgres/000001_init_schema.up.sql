b224f5a1e04e8d4654158a5b7153da992582ebe1
Viacheslav Zalitskyi <slaw.z2014@gmail.com>
2023-03-23 21:17:46 +0200

    migration update

--- migrations/postgres/000001_init_schema.up.sql
+++ migrations/postgres/000001_init_schema.up.sql
@@ -1,6 +1,5 @@
 CREATE TABLE IF NOT EXISTS video_data (
         id SERIAL PRIMARY KEY,
-
         video_id VARCHAR(11) NOT NULL,
         language VARCHAR(2) NOT NULL,
         json_data JSONB NOT NULL
